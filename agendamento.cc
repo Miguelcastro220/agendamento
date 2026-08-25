97768081758164d062e3a02becdd4bcc763c5e1f
Miguel Antonio <MIguelpinho2208@gmail.com>
2026-08-25 11:27:32 -0300

    Update agendamento.cc

--- agendamento.cc
+++ agendamento.cc
@@ -2,8 +2,8 @@
 
 int main(){
 	
-	int dia;
-int nome;
+int dia;
+char(15) nome;
 int ano = 2026;
 int anoNasc;
 int idade;
@@ -25,7 +25,7 @@ idade = ano - anoNasc;
 if (idade <= 12){
 printf("Paciente pode ser atendido");
 }	else  {
-printf("Paciente não tem a idade ideal");
+printf("Paciente nÃ£o tem a idade ideal");
 	return 0;
 }
 
@@ -57,7 +57,7 @@ switch (dia){
 		break;
 }
 
-printf("Valor da consulta é de 150");
+printf("Valor da consulta Ã© de 150");
 printf("Voce pagara o valor no pix ou no cartao");
 
 
@@ -68,7 +68,7 @@ if (cartao == 1)	{
 printf("O valor a ser Pago sera R$150,00");
 }	else {
 	pix = 150 - (150 * 0.10);
-printf ("O valor a ser pago sera R$%.2f");
+printf ("O valor a ser pago sera R$%.2f", pix);
 scanf ("%.2f", &pix);
 	
 }
